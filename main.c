bfedcd644f3faa5c988a61fde06d0a6aec1c9fcb
RudraOP <rudraprasadlugun25@gmail.com>
2026-01-27 16:41:00 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int randomNumber;
+
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate random number between 1 and 100
+    randomNumber = (rand() % 100) + 1;
+    int no_of_guesses=0;
+    int guessed_number;
+    //printf("Random number between 1 and 100: %d\n", randomNumber);
+    //printf("Guess the number:");
+
+    do{
+        printf("Guess the number:");
+        scanf("%d",&guessed_number);
+        if(guessed_number>randomNumber){
+            printf("Lower number please\n");
+        }
+        else if(guessed_number<randomNumber)
+        {
+            printf("Higher number please\n");
+        }
+        else{
+            printf("Congrats!!\n");
+        }
+        no_of_guesses++;
+    }while(guessed_number!=randomNumber);
+
+    printf("You guessed the number in %d guesses\n", no_of_guesses);
+    
+
+    return 0;
+}
